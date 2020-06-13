c8466e99a38b63a35fc3450ab2bc14d29b013bc3
gness1804 <graham.nessler@gmail.com>
2020-06-13 17:31:23 -0500

    Add ability to add info window with hardcoded text

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -18,13 +18,23 @@ export class CustomMap {
     });
   }
 
+  private addInfoWindow (marker: google.maps.Marker): void {
+    const infoWindow = new google.maps.InfoWindow({
+      content: 'Hello',
+    });
+
+    infoWindow.open(this.googleMap, marker);
+  }
+
   addMarker(entity: Entity): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: entity.location.lat,
         lng: entity.location.lng,
       }
     });
+
+    marker.addListener('click', () => this.addInfoWindow(marker));
   }
 }
