09b5e0cf04cafc4d72ddd91593d1876d07100558
gness1804 <graham.nessler@gmail.com>
2020-06-13 14:18:26 -0500

    Add Google Map to app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
+
+const mapWrapper = document.getElementById('map');
+
+new google.maps.Map(mapWrapper, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
